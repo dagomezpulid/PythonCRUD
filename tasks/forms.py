8b919f7d719c9ad898ea155191a3322e3d9cb11b
dagomezpulid <153540824+dagomezpulid@users.noreply.github.com>
2023-12-13 12:21:18 -0500

    commit

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,10 +5,56 @@ from django.forms import ModelForm
 from .models import Task
 
 
-class SignupForm(UserCreationForm):
+class SignUpForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs.update(
+            {
+                "class": "form-input",
+                "required": "",
+                "name": "username",
+                "id": "username",
+                "type": "text",
+                "placeholder": "John Doe",
+                "maxlength": "16",
+                "minlength": "6",
+            }
+        )
+        self.fields["password1"].widget.attrs.update(
+            {
+                "class": "form-input",
+                "required": "",
+                "name": "password1",
+                "id": "password1",
+                "type": "password",
+                "placeholder": "password",
+                "maxlength": "22",
+                "minlength": "8",
+            }
+        )
+        self.fields["password2"].widget.attrs.update(
+            {
+                "class": "form-input",
+                "required": "",
+                "name": "password2",
+                "id": "password2",
+                "type": "password",
+                "placeholder": "password",
+                "maxlength": "22",
+                "minlength": "8",
+            }
+        )
+
+    username = forms.CharField(max_length=20, label=False)
+    email = forms.EmailField(max_length=100)
+
     class Meta:
         model = User
-        fields = ["username", "password1", "password2"]
+        fields = (
+            "username",
+            "password1",
+            "password2",
+        )
 
 
 class LoginForm(forms.Form):
--- tasks/views.py
+++ tasks/views.py
@@ -19,6 +19,10 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            auth_login(request, user)
             return redirect("login")
     else:
         form = UserCreationForm()
